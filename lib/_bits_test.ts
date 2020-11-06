7b5c73ad2cf55c5989e3b7cd11e55a160bca930c
James Bradlee <james@ihacks.dev>
2020-11-06 22:57:58 +0100

    :sparkles: Updated _bits tests.

--- lib/_bits_test.ts
+++ lib/_bits_test.ts
@@ -1,5 +1,5 @@
 // Imports
-import { assert } from "https://deno.land/std@0.74.0/testing/asserts.ts";
+import { assertStrictEquals } from "https://deno.land/std@0.74.0/testing/asserts.ts";
 import {
 	unpack,
 	unpackCount,
@@ -12,118 +12,85 @@ import {
 } from "./_bits.ts";
 
 Deno.test("unpack from start", async () => {
-	assert(
-		unpack(
-			0b10101010n,
-			0n,
-			2n
-		) === 0b10n,
-		"Doesn't match the expected outcome."
+	assertStrictEquals(
+		unpack(0b10101010n, 0n, 2n),
+		0b10n
 	);
 });
 
 Deno.test("unpack from middle", async () => {
-	assert(
-		unpack(
-			0b10101010n,
-			3n,
-			2n
-		) === 0b1n,
-		"Doesn't match the expected outcome."
+	assertStrictEquals(
+		unpack(0b10101010n, 3n, 2n),
+		0b1n
 	);
 });
 
 Deno.test("unpack from end", async () => {
-	assert(
-		unpack(
-			0b10101010n,
-			6n,
-			2n
-		) === 0b10n,
-		"Doesn't match the expected outcome."
+	assertStrictEquals(
+		unpack(0b10101010n, 6n, 2n),
+		0b10n
 	);
 });
 
 Deno.test("pack from start", async () => {
-	assert(
-		pack(
-			0b10101010n,
-			0b01n,
-			0n,
-			2n
-		) === 0b10101001n,
-		"Doesn't match the expected outcome."
+	assertStrictEquals(
+		pack(0b10101010n, 0b01n, 0n, 2n),
+		0b10101001n
 	);
 });
 
 Deno.test("pack from middle", async () => {
-	assert(
-		pack(
-			0b10101010n,
-			0b01n,
-			4n,
-			2n
-		) === 0b10011010n,
-		"Doesn't match the expected outcome."
+	assertStrictEquals(
+		pack(0b10101010n, 0b01n, 4n, 2n),
+		0b10011010n
 	);
 });
 
 Deno.test("pack from end", async () => {
-	assert(
-		pack(
-			0b10101010n,
-			0b01n,
-			6n,
-			2n
-		) === 0b01101010n,
-		"Doesn't match the expected outcome."
+	assertStrictEquals(
+		pack(0b10101010n, 0b01n, 6n, 2n),
+		0b01101010n
 	);
 });
 
 Deno.test("unpack count", async () => {
-	assert(
-		unpackCount(
-			0b110011n
-		) === 0b110011n,
-		"Doesn't match the expected outcome."
+	assertStrictEquals(
+		unpackCount(0b110011n),
+		0b110011n
 	);
 });
 
 Deno.test("unpack shard", async () => {
-	assert(
-		unpackShard(
-			0b11001100000000000000n
-		) === 0b110011n,
-		"Doesn't match the expected outcome."
+	assertStrictEquals(
+		unpackShard(0b11001100000000000000n),
+		0b110011n
 	);
 });
 
 Deno.test("unpack timestamp", async () => {
-	assert(
-		unpackTimestamp(
-			0b11001100000000000000000000n
-		) === 0b110011n,
-		"Doesn't match the expected outcome."
+	assertStrictEquals(
+		unpackTimestamp(0b11001100000000000000000000n),
+		0b110011n
 	);
 });
 
 Deno.test("pack count", async () => {
-	assert(
-		packCount(0n, 1n) === 1n,
-		"Doesn't match the expected outcome."
+	assertStrictEquals(
+		packCount(0n, 1n),
+		1n
 	);
 });
 
 Deno.test("pack shard", async () => {
-	assert(
-		packShard(0n, 1n) === 0b100000000000000n,
-		"Doesn't match the expected outcome."
+	assertStrictEquals(
+		packShard(0n, 1n),
+		0b100000000000000n
 	);
 });
 
 Deno.test("pack timestamp", async () => {
-	assert(
-		packTimestamp(0n, 1n) === 0b100000000000000000000n,
-		"Doesn't match the expected outcome."
+	assertStrictEquals(
+		packTimestamp(0n, 1n),
+		0b100000000000000000000n
 	);
 });
