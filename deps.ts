1eb3cebbd772c3b1bffb898e5808990200830589
James Bradlee <james@ihacks.dev>
2020-11-10 22:01:56 +0100

    :sparkles: Updated dependencies

--- deps.ts
+++ deps.ts
@@ -1,3 +1,4 @@
 // Exports
-export { Base, base62, base10 } from "https://deno.land/x/base@2.0.3/mod.ts";
-export * from "https://deno.land/x/big@1.0.0/mod.ts";
+export type { Big } from "https://deno.land/x/big@1.0.0/mod.ts";
+export { Base, base62, base10 } from "https://deno.land/x/base@2.0.4/mod.ts";
+export { big } from "https://deno.land/x/big@1.0.0/mod.ts";
